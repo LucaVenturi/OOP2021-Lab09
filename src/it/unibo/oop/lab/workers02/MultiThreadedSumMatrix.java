220e4ad26a27ec72f31b00e4f23f59af8223e3f6
Luca <luca.venturi29@studio.unibo.it>
2021-11-23 17:14:22 +0100

    Complete exercise

--- src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.workers02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
@@ -16,9 +19,61 @@ public class MultiThreadedSumMatrix implements SumMatrix {
         this.nthread = nthread;
     }
 
+    private static class Worker extends Thread {
+        private final double[][] matrix;
+        private final int startRow;
+        private final int nRows;
+        private long res;
+
+        Worker(final double[][] matrix, final int startRow, final int nRows) {
+            super();
+            this.matrix = matrix;
+            this.startRow = startRow;
+            this.nRows = nRows;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Working from row " + startRow + " to row " + (startRow + nRows - 1));
+            for (int i = startRow; i < startRow + nRows && i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    this.res += matrix[i][j];
+                }
+            }
+        }
+
+        /**
+         * @return
+         *      Result of the sum of all the values in the matrix.
+         */
+        public long getResult() {
+            return this.res;
+        }
+    }
+
     @Override
     public double sum(final double[][] matrix) {
-        return 0;
+        final int sizeRow = matrix.length % nthread + matrix.length / nthread;
+        final List<Worker> workers = new ArrayList<>(nthread);
+
+        for (int startRow = 0; startRow < matrix.length; startRow += sizeRow) {
+            workers.add(new Worker(matrix, startRow, sizeRow));
+        }
+
+        for (final Thread w:workers) {
+            w.start();
+        }
+
+        long sum = 0;
+        for (final Worker w: workers) {
+            try {
+                w.join();
+                sum += w.getResult();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return sum;
     }
 
 }
